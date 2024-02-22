0cda6827f0e029753fcbcd164e5d43ee77d0ce08
Joseph Huntley <Joseph.Huntley@outlook.com>
2024-02-22 09:22:56 -0500

    Updated error handling for logging and preventing it from being updated to 0

--- UpdateRunningDistance.py
+++ UpdateRunningDistance.py
@@ -11,16 +11,17 @@ payload = {}
 headers = {
     'Authorization': f"Bearer {config_data['s_api_key']}"
 }
+running_distance = 0
 try:
     res = requests.request("GET", config_data['s_url'], headers=headers, data=payload)
-    response.raise_for_status()  # Raise an exception for HTTP errors
-    
+    res.raise_for_status()  # Raise an exception for HTTP errors
+    logging.info(f"Strava API HTTP Status Code: {res.status_code}")
+
     running_distance = round(json.loads(res.text)['ytd_run_totals']['distance'] / 1609.34, 2)
-    logging.info(running_distance)
-except Exception as e
-    logging.error(f"An error has occurred: {e}")
-
-
+    logging.info(f"Running Distance: {running_distance}")
+except Exception as e:
+    logging.error(f"An error has occurred with the Strava api: {e}")
+    
 # Update running distance in Home Assistant
 headers = {
     'Authorization': f'Bearer {config_data["HA_TOKEN"]}',
@@ -34,10 +35,15 @@ entity_id = 'input_number.ytd_running_distance'
 api_endpoint = f'{config_data["HA_URL"]}/states/{entity_id}'
 
 try:
+    if(running_distance == 0):
+        raise ValueError("Running distance is zero")
+
     # Make the POST request to the Home Assistant API with the updated running distance value
     data = json.dumps({"state": running_distance})
     response = requests.post(api_endpoint, headers=headers, data=data)
     response.raise_for_status()  # Raise an exception for HTTP errors
-    logging.info(f"HTTP Status Code: {response.status_code}")
+    logging.info(f"HA API HTTP Status Code: {response.status_code}")
 except requests.exceptions.RequestException as e:
-    logging.error(f"Error updating running distance status: {e}")
+    logging.error(f"Error updating running distance status on HA server: {e}")
+except ValueError as e:
+    logging.error(f"Error: {e}")
