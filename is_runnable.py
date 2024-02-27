6c359f6df49738b6d690b7e891e1058ec3ba2911
Joseph Huntley <Joseph.Huntley@outlook.com>
2024-02-27 12:49:23 -0500

    Added Heat and Sun conditions

--- is_runnable.py
+++ is_runnable.py
@@ -25,18 +25,32 @@ def get_next_forecast_times(forecast_data):
 
     return next_5pm, next_6am
 
-def get_weather_at_time(forecast_data, target_time):
+async def get_weather_at_time(forecast_data, target_time):
     for forecast_day in forecast_data["forecast"]["forecastday"]:
         for hour_data in forecast_day["hour"]:
             forecast_time = datetime.strptime(hour_data["time"], "%Y-%m-%d %H:%M")
             if forecast_time == target_time:
+                sun_value = await get_sun_status(forecast_data, target_time)
                 return {
                     "temp_f": hour_data["temp_f"],
                     "is_rain": hour_data["will_it_rain"],
-                    "is_snow": hour_data["will_it_snow"]
+                    "is_snow": hour_data["will_it_snow"],
+                    "sun": sun_value
                 }
 
     return None
+
+async def get_sun_status(forecast_data, target_time):
+    for forecast_day in forecast_data["forecast"]["forecastday"]:
+        forecast_date = datetime.strptime(forecast_day["date"], "%Y-%m-%d").date()
+        if forecast_date == target_time.date():
+            sunrise_time = datetime.strptime(forecast_day["astro"]["sunrise"], "%I:%M %p")
+            sunset_time = datetime.strptime(forecast_day["astro"]["sunset"], "%I:%M %p")
+
+            logging.info(f"sun: {sunrise_time.time()} {target_time.time()} {sunset_time.time()}")
+
+            return sunrise_time.time() <= target_time.time() <= sunset_time.time()
+
 
 async def get_forecast(days=1, aqi='no', alerts='no'):
     try:
@@ -55,27 +69,35 @@ async def get_forecast(days=1, aqi='no', alerts='no'):
         return None
 
 def get_reason(weather_5pm, weather_6am):
-    # Check if 5 PM is runnable
-    is_runnable_5pm = weather_5pm['temp_f'] >= 55 and weather_5pm['is_rain'] != 1 and weather_5pm['is_snow'] != 1
-    # Generate the reason for 5 PM
-    reason_5pm = None if is_runnable_5pm else (
-        f"\n\t- Temperature is {weather_5pm['temp_f']}°F, too cold." if weather_5pm['temp_f'] < 55 else "",
-        "\n\t- It will rain." if weather_5pm['is_rain'] == 1 else "",
-        "\n\t- It will snow." if weather_5pm['is_snow'] == 1 else ""
-    )
-    # Join the non-empty strings to get the final reason for 5 PM, or set to an empty string if None
-    reason_5pm = " ".join(filter(None, reason_5pm)) if reason_5pm is not None else ""
+    reason_5pm = {
+        "reason": (
+            f"\n\t- Temperature is {weather_5pm['temp_f']}°F", 
+            ", too cold." if weather_5pm['temp_f'] <= 55 else "",
+            ", too hot." if weather_5pm['temp_f'] >= 90 else "",
+            "\n\t- It will rain." if weather_5pm['is_rain'] == 1 else "",
+            "\n\t- It will snow." if weather_5pm['is_snow'] == 1 else "",
+            "\n\t- The sun won't be out." if weather_5pm['sun'] != True  else ""
+        ),
+        "is_runnable": 55 <= weather_5pm['temp_f'] <= 90 and weather_5pm['is_rain'] != 1 and weather_5pm['is_snow'] != 1 and weather_5pm['sun']
+    }
 
-    # Check if 6 AM is runnable
-    is_runnable_6am = weather_6am['temp_f'] > 55 and weather_6am['is_rain'] != 1 and weather_6am['is_snow'] != 1
-    # Generate the reason for 6 AM
-    reason_6am = None if is_runnable_6am else (
-        f"\n\t- Temperature is {weather_6am['temp_f']}°F, too cold." if weather_6am['temp_f'] < 55 else "",
-        "\n\t- It will rain." if weather_6am['is_rain'] == 1 else "",
-        "\n\t- It will snow." if weather_6am['is_snow'] == 1 else ""
-    )
-    # Join the non-empty strings to get the final reason for 6 AM, or set to an empty string if None
-    reason_6am = " ".join(filter(None, reason_6am)) if reason_6am is not None else ""
+    # Join the non-empty strings to get the final reason for 5 PM, or set to an empty string if None
+    reason_5pm["reason"] = "".join(filter(None, reason_5pm["reason"]))
+
+    reason_6am = {
+        "reason": (
+            f"\n\t- Temperature is {weather_6am['temp_f']}°F", 
+            ", too cold." if weather_6am['temp_f'] <= 55 else "",
+            ", too hot." if weather_6am['temp_f'] >= 90 else "",
+            "\n\t- It will rain." if weather_6am['is_rain'] == 1 else "",
+            "\n\t- It will snow." if weather_6am['is_snow'] == 1 else "",
+            "\n\t- The sun won't be out." if weather_6am['sun'] != True  else ""
+        ),
+        "is_runnable": 55 <= weather_6am ['temp_f'] <= 90 and weather_6am['is_rain'] != 1 and weather_6am['is_snow'] != 1 and weather_6am['sun']
+    }
+
+    # Join the non-empty strings to get the final reason for 5 PM, or set to an empty string if None
+    reason_6am["reason"] = "".join(filter(None, reason_6am["reason"]))
 
     return reason_5pm, reason_6am
 
@@ -117,16 +139,17 @@ async def main():
         next_5pm, next_6am = get_next_forecast_times(forecast_data)
         logging.info(f"Next available 5 PM: {next_5pm}, Next available 6 AM: {next_6am}")
 
-        weather_5pm = get_weather_at_time(forecast_data, next_5pm)
-        weather_6am = get_weather_at_time(forecast_data, next_6am)
+        weather_5pm = await get_weather_at_time(forecast_data, next_5pm)
+        weather_6am = await get_weather_at_time(forecast_data, next_6am)
 
         reason_5pm, reason_6am = get_reason(weather_5pm, weather_6am)
 
-        await post_to_ha('input_boolean.runable_5pm', 'on' if reason_5pm == '' else 'off', 'Runnable 5 PM')
-        await post_to_ha('input_text.reason_5pm', reason_5pm, 'Reason 5 PM')
+        await post_to_ha('input_boolean.runable_5pm', 'on' if reason_5pm["is_runnable"] else 'off', 'Runnable 5 PM')
+        await post_to_ha('input_text.reason_5pm', reason_5pm["reason"], 'Reason 5 PM')
 
-        await post_to_ha('input_boolean.runable_6am', 'on' if reason_6am == '' else 'off', 'Runnable 6 AM')
-        await post_to_ha('input_text.reason_6am', reason_6am, 'Reason 6 AM')
+        await post_to_ha('input_boolean.runable_6am', 'on' if reason_6am["is_runnable"] else 'off', 'Runnable 6 AM')
+        await post_to_ha('input_text.reason_6am', reason_6am["reason"], 'Reason 6 AM')
+
 
 if __name__ == "__main__":
     asyncio.run(main())
